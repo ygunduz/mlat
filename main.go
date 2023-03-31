c816e41cf812b3c066d864d68d1c787c8e702d90
yusuf <joshuasmorganos@gmail.com>
2023-03-31 14:28:38 +0300

    windows icon fix

--- main.go
+++ main.go
@@ -5,24 +5,17 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
-	"github.com/wailsapp/wails/v2/pkg/options/linux"
-	"log"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
 
 func main() {
-	icon, err := RetrieveROM("build/appicon.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	// Create an instance of the app structure
 	app := NewApp()
 
 	// Create application with options
-	err = wails.Run(&options.App{
+	err := wails.Run(&options.App{
 		Title:  "MLAT Configuration Tool",
 		Width:  1280,
 		Height: 720,
@@ -33,11 +26,6 @@ func main() {
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
-		},
-		Linux: &linux.Options{
-			Icon:                icon,
-			WindowIsTranslucent: false,
-			WebviewGpuPolicy:    linux.WebviewGpuPolicyAlways,
 		},
 	})
 
