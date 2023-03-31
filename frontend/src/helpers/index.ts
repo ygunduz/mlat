3000df6ba43442b0df6e121fa56a6d72bd0e9b5a
yusuf <joshuasmorganos@gmail.com>
2023-03-31 21:38:02 +0300

    pick position changed

--- frontend/src/helpers/index.ts
+++ frontend/src/helpers/index.ts
@@ -1,5 +1,25 @@
+function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+        return  document.execCommand("copy");
+    } catch (err) {
+        return false;
+    }
+
+    document.body.removeChild(textArea);
+}
+
 export const copyTextToClipboard = (text) => {
     return new Promise((resolve, reject) => {
+        if (!navigator.clipboard) {
+            fallbackCopyTextToClipboard(text);
+            return;
+        }
         navigator?.clipboard?.writeText(text).then(
             function() {
                 resolve(true);
